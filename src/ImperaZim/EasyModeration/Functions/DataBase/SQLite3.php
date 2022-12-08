9c47e8ab61af22be1eab76b259d8a263b1b61238
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-12-07 21:02:43 -0300

    update SQLite3.php

--- src/ImperaZim/EasyModeration/Functions/DataBase/SQLite3.php
+++ src/ImperaZim/EasyModeration/Functions/DataBase/SQLite3.php
@@ -9,10 +9,5 @@ class SQLite3 extends Loader {
  public static function table() {
   return new \SQLite3(self::get()->getDataFolder() . "database.db");
  }
-  
- public static function createTable() : void {
-  self::table()->exec("CREATE TABLE IF NOT EXISTS muted(name TEXT, time INT, reason TEXT, date TEXT, author TEXT)");
-  self::table()->exec("CREATE TABLE IF NOT EXISTS banned(name TEXT, time INT, reason TEXT, date TEXT, author TEXT)");
- } 
  
 }
